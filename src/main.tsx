61a202f4f648c394adc292eb75ac7c7b76941e11
kwakseongjae <gkffhdnls13@gmail.com>
2023-10-05 22:45:59 +0900

    CHORE: react-query / react-query-devtools 설치 및 main.tsx에 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools />
     <App />
-  </React.StrictMode>,
-)
+  </QueryClientProvider>
+);
