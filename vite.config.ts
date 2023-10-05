1d0d423021d0054dfe30ec2faee57211da850f9b
kwakseongjae <gkffhdnls13@gmail.com>
2023-10-05 22:43:17 +0900

    CHORE: Paht Alias 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./",
+  publicDir: "./public",
+  cacheDir: "./.vite",
+  resolve: {
+    alias: [
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@lib", replacement: "/src/lib" },
+      { find: "@assets", replacement: "/src/assets" },
+      { find: "@styles", replacement: "/src/styles" },
+      { find: "@utils", replacement: "/src/utils" },
+    ],
+  },
+});
